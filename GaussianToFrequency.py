ab478dfc4e1fc422579e31d92d31307719958926
emoha <mohamadnet@gmail.com>
2017-08-17 14:18:46 +0430

    Adding fft functions

--- GaussianToFrequency.py
+++ GaussianToFrequency.py
@@ -1,4 +1,11 @@
+'''
+Part of the code is drawn from the Jake VanderPlas (University of Washington's eScience Institute) fft implementation
+http://jakevdp.github.io/blog/2013/08/28/understanding-the-fft/
+'''
+
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 L = 20
 n = 128
@@ -6,3 +13,68 @@ n = 128
 x2 = np.linspace(-L/2, L/2, n+1)
 # Spatial domain
 x = x2[:n]
+u = np.exp(-x**2)
+
+plt.plot(x, u, color='lightblue', linewidth=3)
+plt.xlim(-L/2, L/2)
+plt.show()
+
+
+
+
+def DFT_slow(x):
+    """Compute the discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
+
+def FFT(x):
+    """A recursive implementation of the 1D Cooley-Tukey FFT"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+
+    if N % 2 > 0:
+        raise ValueError("size of x must be a power of 2")
+    elif N <= 32:  # this cutoff should be optimized
+        return DFT_slow(x)
+    else:
+        X_even = FFT(x[::2])
+        X_odd = FFT(x[1::2])
+        factor = np.exp(-2j * np.pi * np.arange(N) / N)
+        return np.concatenate([X_even + factor[:N / 2] * X_odd,
+                               X_even + factor[N / 2:] * X_odd])
+
+
+def FFT_vectorized(x):
+    """A vectorized, non-recursive version of the Cooley-Tukey FFT"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+
+    if np.log2(N) % 1 > 0:
+        raise ValueError("size of x must be a power of 2")
+
+    # N_min here is equivalent to the stopping condition above,
+    # and should be a power of 2
+    N_min = min(N, 32)
+
+    # Perform an O[N^2] DFT on all length-N_min sub-problems at once
+    n = np.arange(N_min)
+    k = n[:, None]
+    M = np.exp(-2j * np.pi * n * k / N_min)
+    X = np.dot(M, x.reshape((N_min, -1)))
+
+    # build-up each level of the recursive calculation all at once
+    while X.shape[0] < N:
+        X_even = X[:, :X.shape[1] / 2]
+        X_odd = X[:, X.shape[1] / 2:]
+        factor = np.exp(-1j * np.pi * np.arange(X.shape[0])
+                        / X.shape[0])[:, None]
+        X = np.vstack([X_even + factor * X_odd,
+                       X_even - factor * X_odd])
+
+    return X.ravel()
+
